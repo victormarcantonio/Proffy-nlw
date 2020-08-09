d9410ed8934693afebf72f23771e28e4bd5c0f41
unknown <victor_marcantonio@hotmail.com>
2020-08-09 13:17:11 -0300

    subindo repositorio para o github

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Routes from './routes';
+
+import Landing from './pages/Landing';
+
+function App() {
+  return (
+     <Routes/>
+  );
+}
+
+export default App;
